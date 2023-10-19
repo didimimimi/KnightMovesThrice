c75381c5bebf20e374021c5831423b4a4cd59517
Dimitris Tasios <dtasios4@gmail.com>
2023-10-20 01:22:37 +0300

    feat: add KnightHelper file

---
+++ KnightMovesThrice/KnightMovesThrice/KnightHelper.swift
@@ -0,0 +1,13 @@
+//
+//  KnightHelper.swift
+//  KnightMovesThrice
+//
+//  Created by  Dimitris Tasios Personal on 20/10/23.
+//
+
+import Foundation
+
+struct KnightPosition: Hashable {
+    let x: Int
+    let y: Int
+}
