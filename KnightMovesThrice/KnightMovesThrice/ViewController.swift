61680ff77b47561221733edbc9bf465aca44211c
Dimitris Tasios <dtasios4@gmail.com>
2023-10-20 01:06:41 +0300

    build: initial commit for Xcode project

---
+++ KnightMovesThrice/KnightMovesThrice/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  KnightMovesThrice
+//
+//  Created by  Dimitris Tasios Personal on 20/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
